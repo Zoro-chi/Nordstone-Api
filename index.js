e12ac7e8b63a4c9371bdee62802192d4e63c941d
zoro <jagbetuyi001@gmail.com>
2022-11-15 22:41:02 +0100

    updated api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 // CALC ROUTE
@@ -27,7 +27,11 @@ app.post("/api/calc", (req, res) => {
       break;
   }
 
-  res.status(200).json({ answer });
+  if (num1 !== "" && num2 !== "" && operator !== "") {
+    res.status(200).json({ answer: parseFloat(answer.toFixed(2)) });
+  } else {
+    res.status(500).json("please make sure all fields are filled correctly");
+  }
 });
 
 const PORT = 8080;
