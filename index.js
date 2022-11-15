acc7328dc3ad2c4dce8b1cbb8f0d94c4dc80e61d
zoro <jagbetuyi001@gmail.com>
2022-11-15 14:48:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// CALC ROUTE
+app.post("/api/calc", (req, res) => {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  const operator = req.body.operator;
+  let answer;
+
+  switch (operator) {
+    case "+":
+      answer = num1 + num2;
+      break;
+
+    case "-":
+      answer = num1 - num2;
+      break;
+
+    case "*":
+      answer = num1 * num2;
+      break;
+  }
+
+  res.status(200).json({ answer });
+});
+
+const PORT = 8080;
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
